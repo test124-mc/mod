a6a491337fdcb3d49ee3066c682b5ab24388ae3c
notcash <test124@pop-os.home>
2023-12-30 11:14:26 +0000

    my mod

---
+++ src/main/java/net/name/modid/Test124Client.java
@@ -0,0 +1,8 @@
+package net.name.modid;
+import net.fabricmc.api.ClientModInitializer;
+public class Test124Client implements net.fabricmc.api.ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/name/modid/Test124DataGenerator.java
@@ -0,0 +1,11 @@
+package net.name.modid;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class Test124DataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
